ec7160b58bbcbede17b8f2b3d906963df4dc7c36
Issac Chew <issac_chew870@hotmail.com>
2021-04-30 20:07:41 +0800

    Update read_file.py

--- read_file.py
+++ read_file.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
-data = pd.read_excel('Database.xlsx', sheet_name = None)
+df = pd.read_excel(r"Database.xlsx", sheet_name = 'GENERAL_INTENTS', index_col= 0).to_dict(orient='index')
+df2 = pd.read_excel(r"Database.xlsx", sheet_name = 'FAQS', index_col= 0).to_dict(orient='index')
+df3 = pd.read_excel(r"Database.xlsx", sheet_name = 'DEFAULT_REPLY', index_col= 0).to_dict(orient='index')
 
-for key in data:
-     print(data[key].head())
+df, df2, df3
+#print(f"1: {df} \n2: {df2} \n3: {df3} ")
