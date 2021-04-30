14a37cde316c001e303418ee2580eddfacdaa2ed
Brandon Tan <brandontan99@ymail.com>
2021-04-30 18:48:36 +0800

    Added color in chatbot reply

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from termcolor import colored
 from entity_extraction import get_entities
 from text_preprocessing import get_corpus as get_preprocessed_text
 
@@ -6,9 +7,14 @@ GENERAL_INTENT = 1
 FAQS = 2
 DEFAULT_REPLY = 3
 
+def display_chatbot_reply(reply):
+    reply = "Chatbot: "
+    reply += colored(reply+"\n","green")
+    print(reply)
+
 if __name__ == '__main__':
     # Start of Chatbot
-    print("Hi, how can I help you?")
+    display_chatbot_reply("Hi, how can I help you? \n")
     while True:
         user_message = input()  
         if user_message == "q":
@@ -29,13 +35,13 @@ if __name__ == '__main__':
             highest_confid_lvl_ans = highest_confid_lvl_ans.replace("NAME", entities["PERSON"][0])
             highest_confid_lvl_ans = highest_confid_lvl_ans.replace("BANK_ACC", entities["BANK_ACC"][0])
             highest_confid_lvl_ans = highest_confid_lvl_ans.replace("AMOUNT", entities["AMOUNT"][0])
-            print(highest_confid_lvl_ans)
+            display_chatbot_reply(highest_confid_lvl_ans)
 
         elif match_type == FAQS:
-            print(highest_confid_lvl_ans)
+            display_chatbot_reply(highest_confid_lvl_ans)
 
         elif match_type == DEFAULT_REPLY:
-            print(highest_confid_lvl_ans)
+            display_chatbot_reply(highest_confid_lvl_ans)
 
     # End of Chatbot
-    print("Thank you for using our chatbot.")
+    display_chatbot_reply("Thank you for using our chatbot.")
